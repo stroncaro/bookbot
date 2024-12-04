25e91f1b736f181406ca4f354d6822ed2135e35c
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-12-04 08:38:24 -0300

    add get_word_count method

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_contents = get_book_contents(book_path)
-    print(book_contents)
+    book_word_count = get_word_count(book_contents)
+    print(book_word_count)
 
 
 def get_book_contents(path):
     with open(path) as f:
         return f.read()
+
+
+def get_word_count(text):
+    return len(text.split())
 
 
 main()
